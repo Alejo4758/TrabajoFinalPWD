90074f53864417adfef5aed6213735eb242dbf01
Alejo Lopez <LopezAlejo0124@gmail.com>
2025-11-17 16:33:40 -0300

    Carpeta para estilos

--- Includes/head.php
+++ Includes/head.php
@@ -1,11 +1,11 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!-- TITULO -->
-<title>Perfumeria Tu Esencia</title>
+<!-- Título -->
+<title>Perfumería Tu Esencia</title>
 
-<!-- estilos libreria -->
-<link rel="stylesheet" href="../estilos.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<!-- Estilos librería -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
-<!-- estilos css -->
- <link rel="stylesheet" href="estilos.css">
+<!-- Estilos css -->
+ <link rel="stylesheet" href="../Vista/css/estilos.css">
